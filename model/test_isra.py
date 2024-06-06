01ef9d45367be68d1856e8de80b96e92d91eb4e9
Diego CH <158114096+diego-ch-21@users.noreply.github.com>
2024-06-05 22:34:39 -0500

    modificacion_test

--- model/test_isra.py
+++ model/test_isra.py
@@ -3,18 +3,17 @@ from datetime import date
 from dataclasses import dataclass
 
 @dataclass
-class Test(db.Model):
-    __tablename__='test'
-    id_test = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class Test_isra(db.Model):
+    __tablename__='test_isra'
+    id_test_isra = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_estudiante = db.Column(db.Integer, db.ForeignKey('estudiante.id_estudiante'), nullable=False)
-    clasificacion= db.Column(db.String(100))
-    total_test = db.Column(db.Integer)
     fecha = db.Column(db.Date)
+    punt_test = db.Column(db.Integer)
 
     estudiante = db.relationship('Estudiante', backref='tests')
     
-    def __init__(self, id_estudiante, clasificacion, total_test, fecha):
+    def __init__(self,id_test_isra, id_estudiante, fecha, punt_test):
+        self.id_test_isra = id_test_isra
         self.id_estudiante = id_estudiante
-        self.clasificacion = clasificacion
-        self.total_test = total_test
         self.fecha = fecha
+        self.punt_test = punt_test
