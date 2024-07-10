baee108b9d315ddf74a1d8c27395e90975c4d603
Diego CH <158114096+diego-ch-21@users.noreply.github.com>
2024-07-10 14:30:46 -0500

    nuevo microservicio

---
+++ v2-microservicios/realizar-test-service/src/main/java/com/realizar_test_service/realizar_test_service/RealizarTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.realizar_test_service.realizar_test_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealizarTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealizarTestServiceApplication.class, args);
+	}
+
+}
---
+++ v2-microservicios/realizar-test-service/src/test/java/com/realizar_test_service/realizar_test_service/RealizarTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realizar_test_service.realizar_test_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealizarTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
